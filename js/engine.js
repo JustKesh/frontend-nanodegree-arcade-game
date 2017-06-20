0d39d4ec83d43d0ae3b0da84983d03e19783069b
JustKesh <keshia.n.clarke@gmail.com>
2017-06-20 09:11:46 -0400

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -80,7 +80,6 @@ var Engine = (function(global) {
      */
     function update(dt) {
         updateEntities(dt);
-        // checkCollisions();
     }
 
     /* This is called by the update function and loops through all of the
@@ -171,7 +170,7 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-princess-girl.png'
     ]);
     Resources.onReady(init);
 
